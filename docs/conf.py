bfbce6cd4853f34b40f046dbcd6887b30ea8b9f9
Brian Hodges <bhodges@zulily.com>
2015-11-17 18:57:06 -0800

    mock pyyaml, remove python-ldap and pyyaml from requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['pyhon-ldap']
+MOCK_MODULES = ['pyhon-ldap', 'pyyaml']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
